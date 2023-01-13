d192dbccd4c9d6085b9c188341a3201005a32319
Liusj <1690150504@qq.com>
2023-01-13 18:18:25 +0800

    fix:rollup依赖问题

--- src/views/map/leaflet/index.tsx
+++ src/views/map/leaflet/index.tsx
@@ -6,7 +6,7 @@ import { Slider } from 'antd'
 import 'leaflet.chinatmsproviders'
 import 'leaflet.markercluster/dist/leaflet.markercluster'
 import './markercluster.css'
-import './MarkerCluster.Default.css'
+import './markercluster.default.css'
 
 const Select = ({ start, end, address }: { start: string, end: string, address: [] }) => {
   return (
